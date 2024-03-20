b43d0122ea6ebe56b625856271513111190403df
Kouros26 <k.lotfi@student.isartdigital.com>
2024-03-20 16:46:46 +0100

    Added knight + started IA movement

--- Assets/Script/Units/Golem.cs
+++ Assets/Script/Units/Golem.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Golem : Unit
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        base.Start();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        base.Update();
     }
 }
--- Assets/Script/Units/Unit.cs
+++ Assets/Script/Units/Unit.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    public Camera Camera;
+    public UnityEngine.AI.NavMeshAgent Agent;
+    public Animator Animator;
+
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        
+        Camera = Camera.main;
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out var hit))
+            {
+                Agent.SetDestination(hit.point);
+            }
+        }
+
+        Animator.SetFloat("Speed", Agent.velocity.magnitude);
     }
 }
